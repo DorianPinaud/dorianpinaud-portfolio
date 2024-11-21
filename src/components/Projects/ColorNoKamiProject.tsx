fab8b35d8244e52eeff46134a6e54e0e879de733
dorian.pinaud <dorian.pinaud@gmail.com>
2024-11-21 22:46:47 +0100

    add color no kami

--- src/components/Projects/ColorNoKamiProject.tsx
+++ src/components/Projects/ColorNoKamiProject.tsx
@@ -14,8 +14,8 @@ export default function ColorNoKamiProject() {
                     <i className="fa-regular fa-square-check"></i>
                     U-Net Powered by MangaDex API
                 </p>
-                <p><em>Work in progress</em></p>
             </div>
+            <a className='btn' href='https://github.com/DorianPinaud/colorNoKami'>Go Check</a>
         </div>
     )
 }
